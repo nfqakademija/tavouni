f93dbdc7675aeb0bb24de3c70dc885b746f92cef
Aurimas <aurimas1rimkus@gmail.com>
2017-11-04 23:29:39 +0200

    Add Room entity

---
+++ src/AppBundle/Entity/Room.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Room
+ *
+ * @ORM\Table(name="room")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\RoomRepository")
+ */
+class Room
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="no", type="string", length=255)
+     */
+    private $no;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="buildingId", type="integer")
+     */
+    private $buildingId;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set no
+     *
+     * @param string $no
+     *
+     * @return Room
+     */
+    public function setNo($no)
+    {
+        $this->no = $no;
+
+        return $this;
+    }
+
+    /**
+     * Get no
+     *
+     * @return string
+     */
+    public function getNo()
+    {
+        return $this->no;
+    }
+
+    /**
+     * Set buildingId
+     *
+     * @param integer $buildingId
+     *
+     * @return Room
+     */
+    public function setBuildingId($buildingId)
+    {
+        $this->buildingId = $buildingId;
+
+        return $this;
+    }
+
+    /**
+     * Get buildingId
+     *
+     * @return int
+     */
+    public function getBuildingId()
+    {
+        return $this->buildingId;
+    }
+}
+
