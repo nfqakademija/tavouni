528ed5d842780f2adea68c8f523f080d8c7acf30
IgnasZdanis <zdanis.ignas@gmail.com>
2017-11-06 20:35:21 +0200

    Creating events for each lecture

--- src/AppBundle/Listener/LoadDataListener.php
+++ src/AppBundle/Listener/LoadDataListener.php
@@ -3,11 +3,32 @@
 namespace AppBundle\Listener;
 
 use AncaRebeca\FullCalendarBundle\Event\CalendarEvent;
+use AncaRebeca\FullCalendarBundle\Model\Event;
 use AncaRebeca\FullCalendarBundle\Model\FullCalendarEvent;
-use AppBundle\Entity\CalendarEvent as MyCustomEvent;
+use AppBundle\Entity\Group;
+use AppBundle\Entity\Lecture;
+use AppBundle\Entity\LectureDate;
+use AppBundle\Entity\Lecturer;
+use AppBundle\Entity\Room;
+use AppBundle\Entity\Student;
+use AppBundle\Entity\Subject;
+use AppBundle\Repository\LectureDateRepository;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 
 class LoadDataListener
 {
+    /**
+     * @var EntityManager
+     */
+    private $em;
+    private $tokenStorage;
+
+    public function __construct(EntityManager $em, TokenStorage $tokenStorage)
+    {
+        $this->em = $em;
+        $this->tokenStorage = $tokenStorage;
+    }
     /**
      * @param CalendarEvent $calendarEvent
      *
@@ -15,13 +36,21 @@ class LoadDataListener
      */
     public function loadData(CalendarEvent $calendarEvent)
     {
-        $startDate = $calendarEvent->getStart();
-        $endDate = $calendarEvent->getEnd();
-        $filters = $calendarEvent->getFilters();
-
-        //You may want do a custom query to populate the events
-
-        $calendarEvent->addEvent(new MyCustomEvent('Event Title 1', new \DateTime()));
-        $calendarEvent->addEvent(new MyCustomEvent('Event Title 2', new \DateTime()));
+        $date = new \DateTime();
+        $date->setDate(2017, 11, 06);
+        $date->setTime(12, 0);
+        $date2 = new \DateTime();
+        $date2->setDate(2017, 11, 06);
+        $date2->setTime(14, 0);
+        $user = $this->tokenStorage->getToken()->getUser()->getId();
+        $studentLectures = $this->em->getRepository('AppBundle\Entity\LectureDate')->getLectureDatesByUser($user);
+        foreach($studentLectures as $lecture)
+        {
+            $data = "\n".$lecture->getLecture()->getSubject()->getName()."\n".$lecture->getLecture()->getSubject()->getSubjectType()."\n".$lecture->getLecture()->getLecturer()->getName();
+            $event = new Event($data, $lecture->getDate());
+            $event->setEndDate($date2);
+            $event->setAllDay(false);
+            $calendarEvent->addEvent($event);
+        }
     }
 }
