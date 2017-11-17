525b71806c31812c714aa30dad9bce387bded659
Aurimas <aurimas1rimkus@gmail.com>
2017-11-17 13:27:48 +0200

    Add Lecturer News routes

--- src/AppBundle/Controller/Lecturer/NewsController.php
+++ src/AppBundle/Controller/Lecturer/NewsController.php
@@ -15,4 +15,20 @@ class NewsController extends Controller
     {
         return $this->render('', array('name' => $name));
     }
+
+    /**
+     * @Route("/posts", name="lecturer_posts")
+     */
+    public function showPostsAction()
+    {
+        return $this->render(':Lecturer/News:show_posts.html.twig');
+    }
+
+    /**
+     * @Route("/posts/new", name="lecturer_new_post")
+     */
+    public function addPostAction()
+    {
+        return $this->render(':Lecturer/News:add_post.html.twig');
+    }
 }
