33eff4d19223554604315a90ef062a0915a84155
IgnasZdanis <zdanis.ignas@gmail.com>
2017-11-18 14:40:58 +0200

    Created lecturer controller

---
+++ src/AppBundle/Controller/Lecturer/LecturerController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ignas
+ * Date: 17.11.18
+ * Time: 13.18
+ */
+
+namespace AppBundle\Controller\Lecturer;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class LecturerController extends Controller
+{
+    /**
+     * @Route("/lecturer", name="lecturer")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render(
+            'Lecturer/lecturer_homepage.html.twig'
+        );
+    }
+}
