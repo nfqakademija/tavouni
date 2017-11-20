59530a1db6f615ca2cea63064da7f39714026b0c
IgnasZdanis <zdanis.ignas@gmail.com>
2017-11-20 17:10:25 +0200

    Added route for homepage, changed route for timetable

--- src/AppBundle/Controller/Student/StudentController.php
+++ src/AppBundle/Controller/Student/StudentController.php
@@ -8,16 +8,32 @@
 
 namespace AppBundle\Controller\Student;
 
+use AppBundle\Repository\PostRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 
 class StudentController extends Controller
 {
     /**
      * @Route("/student", name="student")
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request, TokenStorage $tokenStorage, PostRepository $postRepository)
+    {
+        $id = $tokenStorage->getToken()->getUser()->getId();
+        $posts = $postRepository->getPostsForStudent(21);
+        // replace this example code with whatever you need
+        return $this->render(
+            'Student/student_homepage.html.twig', [
+                'posts' =>$posts
+            ]
+        );
+    }
+    /**
+     * @Route("/timetable", name="timetable")
+     */
+    public function timetableAction(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render(
