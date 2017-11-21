0aa8ff6d09f59477014792d7e22aaa7e19039594
Aurimas <aurimas1rimkus@gmail.com>
2017-11-21 21:26:29 +0200

    Add redirecting from login to index when already logged in

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -9,6 +9,13 @@ class SecurityController extends BaseSecurityController
 {
     public function loginAction(Request $request)
     {
+        if ($this->isGranted('ROLE_STUDENT')) {
+            return $this->redirectToRoute('student_index');
+        }
+
+        if ($this->isGranted('ROLE_LECTURER')) {
+            return $this->redirectToRoute('lecturer_index');
+        }
         return parent::loginAction($request);
     }
 }
