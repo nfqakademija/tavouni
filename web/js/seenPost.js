fe73bca97198e3c1273f7442d0a5b0040c3db280
Aurimas <aurimas1rimkus@gmail.com>
2017-11-23 00:08:39 +0200

    Add blinking when post is unseen

--- web/js/seenPost.js
+++ web/js/seenPost.js
@@ -1,7 +1,9 @@
 $("a.title").click(function () {
     var panel = $(this).closest(".timeline-panel");
+    var icon = panel.prev(".timeline-badge").children("i");
     if (panel.hasClass("bg-info")) {
         panel.removeClass("bg-info");
+        icon.removeClass("blink");
         $.ajax ({
             url: "/student/set_post_seen",
             type: "POST",
