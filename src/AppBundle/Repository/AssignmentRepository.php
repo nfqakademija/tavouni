c603e91d98cb08a1861eae7988115d7aa3553555
Aurimas <aurimas1rimkus@gmail.com>
2017-11-23 13:41:03 +0200

    Add database query for getting student assignment grades average

--- src/AppBundle/Repository/AssignmentRepository.php
+++ src/AppBundle/Repository/AssignmentRepository.php
@@ -10,4 +10,20 @@ namespace AppBundle\Repository;
  */
 class AssignmentRepository extends \Doctrine\ORM\EntityRepository
 {
+    public function getAssignmentsGradesAverageByStudentGroup($id)
+    {
+        return $this->_em->createQuery("SELECT a1, su, AVG(g1.value)
+            FROM AppBundle\Entity\Assignment a1
+            JOIN a1.grades g1
+            JOIN a1.subject su
+            WHERE a1 IN ( 
+              SELECT a2
+              FROM AppBundle\Entity\Assignment a2
+              JOIN a2.grades g2
+              JOIN g2.student st
+              JOIN st.user u
+              WHERE u.id = :id)
+            GROUP BY a1
+          ")->setParameter('id', $id)->getResult();
+    }
 }
