2d0c26b7df9f78be8441eb61593ee0b838fd8b09
Aurimas <aurimas1rimkus@gmail.com>
2017-12-17 12:19:12 +0200

    Add assignment events to calendar file

--- src/AppBundle/Controller/CalendarDownloadController.php
+++ src/AppBundle/Controller/CalendarDownloadController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Repository\AssignmentEventRepository;
 use AppBundle\Repository\LectureDateRepository;
 use AppBundle\Utils\VCalendarGenerator;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -16,7 +17,8 @@ class CalendarDownloadController extends Controller
      */
     public function downloadAction(
         VCalendarGenerator $vCalendarGenerator,
-        LectureDateRepository $ldRepository
+        LectureDateRepository $ldRepository,
+        AssignmentEventRepository $aeRepository
     ): Response {
         $response = new Response();
         $response->headers->set('Content-Type', 'text/calendar');
@@ -24,10 +26,16 @@ class CalendarDownloadController extends Controller
 
         if ($this->isGranted('ROLE_STUDENT')) {
             $lectureDates = $ldRepository->getLectureDatesByStudent($this->getUser()->getId());
+            $assignmentEvents = $aeRepository->getAssignmentEventsForStudent($this->getUser()->getId());
         } elseif ($this->isGranted('ROLE_LECTURER')) {
             $lectureDates = $ldRepository->getLectureDatesByLecturer($this->getUser()->getId());
+            $assignmentEvents = $aeRepository->getAssignmentEventsForLecturer($this->getUser()->getId());
         }
-        $content = $vCalendarGenerator->generateVCalendarContent($lectureDates, $this->isGranted('ROLE_STUDENT'));
+        $content = $vCalendarGenerator->generateVCalendarContent(
+            $lectureDates,
+            $assignmentEvents,
+            $this->isGranted('ROLE_STUDENT')
+        );
         $response->setContent($content);
 
         return $response;
