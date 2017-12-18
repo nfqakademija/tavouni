d394e11a5dbbcac0cb266992ba7590c750cf1b9b
Aurimas <aurimas1rimkus@gmail.com>
2017-12-18 22:38:18 +0200

    Add lithuanian language to DataTables

--- web/js/dataTables.js
+++ web/js/dataTables.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-    $('.table').DataTable();
+    $('.table').DataTable({
+        "language": {
+            "url": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/Lithuanian.json"
+        }
+    });
 });
