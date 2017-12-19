13f53e5bd02397783085d13b69dd7dabaef33d93
Aurimas <aurimas1rimkus@gmail.com>
2017-12-19 20:23:54 +0200

    Optimize duplicate code

--- src/AppBundle/DataFixtures/ORM/UserFixtures.php
+++ src/AppBundle/DataFixtures/ORM/UserFixtures.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 class UserFixtures extends Fixture
 {
+    public static $students;
     public static $lecturers;
 
     /**
@@ -22,35 +23,134 @@ class UserFixtures extends Fixture
 
     private function loadStudents(ObjectManager $manager)
     {
-        $students = [
-            ['firstName' => 'Ignas', 'lastName' => 'Zdanis'],
-            ['firstName' => 'Ignas', 'lastName' => 'Kvietkus'],
-            ['firstName' => 'Ignas', 'lastName' => 'Ivoška'],
-            ['firstName' => 'Aurimas', 'lastName' => 'Rimkus'],
-            ['firstName' => 'Tadas', 'lastName' => 'Adomaitis'],
-            ['firstName' => 'Vilius', 'lastName' => 'Žukauskas'],
-            ['firstName' => 'Tomas', 'lastName' => 'Ūselis'],
-            ['firstName' => 'Dovydas', 'lastName' => 'Skrebė'],
-            ['firstName' => 'Saulius', 'lastName' => 'Skliutas'],
-            ['firstName' => 'Andrius', 'lastName' => 'Paulauskas'],
-            ['firstName' => 'Lukas', 'lastName' => 'Andriejūnas'],
-            ['firstName' => 'Tomas', 'lastName' => 'Germanavičius'],
-            ['firstName' => 'Gerda', 'lastName' => 'Šimoliūnaitė'],
-            ['firstName' => 'Greta', 'lastName' => 'Mameniškytė'],
-            ['firstName' => 'Rytis', 'lastName' => 'Kaplūnas'],
-            ['firstName' => 'Domantas', 'lastName' => 'Pelaitis'],
-            ['firstName' => 'Domantas', 'lastName' => 'Jadenkus'],
-            ['firstName' => 'Domantas', 'lastName' => 'Lekavičius'],
-            ['firstName' => 'Lukas', 'lastName' => 'Valatka'],
-            ['firstName' => 'Solveiga', 'lastName' => 'Benediktavičiūtė'],
-            ['firstName' => 'Greta', 'lastName' => 'Griškaitytė'],
-            ['firstName' => 'Viktorija', 'lastName' => 'Kazokaitytė'],
-            ['firstName' => 'Reda', 'lastName' => 'Kviekutė'],
-            ['firstName' => 'Jovaras', 'lastName' => 'Ivoška'],
-            ['firstName' => 'Jurgita', 'lastName' => 'Paulauskaitė'],
+        $this::$students = [
+            [
+                'firstName' => 'Ignas',
+                'lastName' => 'Zdanis',
+                'groupsRefs' => ['PS3k', 'PS3k3g', 'SM', 'SM1g', 'PST2g', 'FUN']
+            ],
+            [
+                'firstName' => 'Ignas',
+                'lastName' => 'Kvietkus',
+                'groupsRefs' => ['PS3k', 'PS3k3g', 'SM', 'SM1g', 'PST2g', 'LOG']
+            ],
+            [
+                'firstName' => 'Ignas',
+                'lastName' => 'Ivoška',
+                'groupsRefs' => ['PS3k', 'PS3k3g', 'SM', 'SM1g', 'PST2g', 'LOG']
+            ],
+            [
+                'firstName' => 'Aurimas',
+                'lastName' => 'Rimkus',
+                'groupsRefs' => ['PS3k', 'PS3k3g', 'APP', 'APP2g', 'PST2g', 'KOM']
+            ],
+            [
+                'firstName' => 'Tadas',
+                'lastName' => 'Adomaitis',
+                'groupsRefs' => ['PS3k', 'PS3k3g', 'SM', 'SM1g', 'PST2g', 'LOG']
+            ],
+            [
+                'firstName' => 'Vilius',
+                'lastName' => 'Žukauskas',
+                'groupsRefs' => ['PS3k', 'PS3k6g', 'SM', 'SM1g', 'PST1g', 'LOG'],
+            ],
+            [
+                'firstName' => 'Tomas',
+                'lastName' => 'Ūselis',
+                'groupsRefs' => ['PS3k', 'PS3k3g', 'APP', 'APP2g', 'PST2g']
+            ],
+            [
+                'firstName' => 'Dovydas',
+                'lastName' => 'Skrebė',
+                'groupsRefs' => ['PS3k', 'PS3k3g', 'APP', 'APP2g', 'PST2g']
+            ],
+            [
+                'firstName' => 'Saulius',
+                'lastName' => 'Skliutas',
+                'groupsRefs' => ['PS3k', 'PS3k3g', 'KOD', 'APP', 'PST2g']
+            ],
+            [
+                'firstName' => 'Andrius',
+                'lastName' => 'Paulauskas',
+                'groupsRefs' => ['PS3k', 'PS3k3g', 'KOD', 'PST2g']
+            ],
+            [
+                'firstName' => 'Lukas',
+                'lastName' => 'Andriejūnas',
+                'groupsRefs' => ['PS3k', 'PS3k3g', 'APP', 'APP2g', 'KOD', 'PST2g']
+            ],
+            [
+                'firstName' => 'Tomas',
+                'lastName' => 'Germanavičius',
+                'groupsRefs' => ['PS3k', 'PS3k3g', 'KOD', 'PST2g']
+            ],
+            [
+                'firstName' => 'Gerda',
+                'lastName' => 'Šimoliūnaitė',
+                'groupsRefs' => ['PS3k', 'PS3k3g', 'FUN', 'KOM', 'PST2g']
+            ],
+            [
+                'firstName' =>'Greta',
+                'lastName' => 'Mameniškytė',
+                'groupsRefs' => ['PS3k', 'PS3k3g', 'APP', 'APP1g', 'KOD', 'PST2g']
+            ],
+            [
+                'firstName' => 'Rytis',
+                'lastName' => 'Kaplūnas',
+                'groupsRefs' => ['PS3k', 'PS3k6g', 'APP', 'APP1g', 'KOM', 'PST1g']
+            ],
+            [
+                'firstName' => 'Domantas',
+                'lastName' => 'Pelaitis',
+                'groupsRefs' => ['PS3k', 'PS3k3g', 'APP', 'APP2g', 'KOD', 'PST2g']
+            ],
+            [
+                'firstName' => 'Domantas',
+                'lastName' => 'Jadenkus',
+                'groupsRefs' => ['PS3k', 'PS3k3g', 'APP', 'APP1g', 'FUN', 'PST2g']
+            ],
+            [
+                'firstName' => 'Domantas',
+                'lastName' => 'Lekavičius',
+                'groupsRefs' => ['PS3k', 'PS3k6g', 'FUN', 'SM', 'SM1g', 'PST1g']
+            ],
+            [
+                'firstName' => 'Lukas',
+                'lastName' => 'Valatka',
+                'groupsRefs' => ['PS3k', 'PS3k6g', 'LOG', 'KOD', 'PST1g']
+            ],
+            [
+                'firstName' => 'Solveiga',
+                'lastName' => 'Benediktavičiūtė',
+                'groupsRefs' => ['PS3k', 'PS3k6g', 'FUN', 'SM', 'SM1g', 'PST1g']
+            ],
+            [
+                'firstName' => 'Greta',
+                'lastName' => 'Griškaitytė',
+                'groupsRefs' => ['PS3k', 'PS3k6g', 'FUN', 'KOD', 'PST1g']
+            ],
+            [
+                'firstName' => 'Viktorija',
+                'lastName' => 'Kazokaitytė',
+                'groupsRefs' => ['PS3k', 'PS3k3g', 'LOG', 'SM', 'SM1g', 'PST2g']
+            ],
+            [
+                'firstName' => 'Reda',
+                'lastName' => 'Kviekutė',
+                'groupsRefs' => ['PS3k', 'PS3k6g', 'FUN', 'KOM', 'PST1g']
+            ],
+            [
+                'firstName' => 'Jovaras',
+                'lastName' => 'Ivoška',
+                'groupsRefs' => ['PS3k', 'PS3k6g', 'LOG', 'SM', 'SM1g', 'PST1g']
+            ],
+            [
+                'firstName' => 'Jurgita',
+                'lastName' => 'Paulauskaitė',
+                'groupsRefs' => ['PS3k', 'PS3k3g', 'LOG', 'SM', 'SM1g', 'PST2g']
+            ],
         ];
-
-        $this->persistPeople($manager, $students, true);
+        $this->persistPeople($manager, $this::$students, true);
     }
 
     private function loadLecturers(ObjectManager $manager)
@@ -70,7 +170,6 @@ class UserFixtures extends Fixture
             ['firstName' => 'Mindaugas', 'lastName' => 'Karpinskas'],
             ['firstName' => 'Giedrius', 'lastName' => 'Graževičius'],
         ];
-
         $this->persistPeople($manager, $this::$lecturers, false);
     }
 
