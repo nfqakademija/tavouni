6f8b8481c288f7df4e9df76d3f19666b095146db
IgnasZdanis <zdanis.ignas@gmail.com>
2017-12-19 15:37:31 +0200

    Room and building fixtures

--- src/AppBundle/DataFixtures/ORM/RoomFixtures.php
+++ src/AppBundle/DataFixtures/ORM/RoomFixtures.php
@@ -13,18 +13,46 @@ class RoomFixtures extends Fixture
      */
     public function load(ObjectManager $manager)
     {
-        $manager->persist($this->createRoom('102', 'Didl', '102didl'));
-        $manager->persist($this->createRoom('101', 'Didl', '101didl'));
-        $manager->persist($this->createRoom('101', 'Naug', '101naug'));
+//        $manager->persist($this->createRoom('102', 'Didl'));
+//        $manager->persist($this->createRoom('101', 'Didl'));
+//        $manager->persist($this->createRoom('101', 'Naug'));
+        $this->createRooms('didl', $manager);
+        $this->createRooms('naug', $manager);
+        $this->createRooms('CH', $manager);
+        $this->createRooms('salt', $manager);
+        $this->createRooms('KF', $manager);
+        $this->createRooms('FF', $manager);
+        $this->createRooms('FL', $manager);
+        $this->createRooms('IS', $manager);
+        $this->createRooms('FZ', $manager);
+        $this->createRooms('GM', $manager);
+        $this->createRooms('MED', $manager);
         $manager->flush();
     }
 
-    private function createRoom(string $no, string $buildingRef, string $reference): Room
+    private function createRoom(string $no, string $buildingRef): Room
     {
         $room = new Room($no, $this->getReference($buildingRef));
-        $this->addReference($reference, $room);
+        $this->addReference($no . $buildingRef, $room);
 
         return $room;
+    }
+
+    private function createRooms(string $buildingRef, ObjectManager $manager)
+    {
+        $rooms = [];
+        $rooms[] = $this->createRoom('101', $buildingRef);
+        $rooms[] = $this->createRoom('102', $buildingRef);
+        $rooms[] = $this->createRoom('201', $buildingRef);
+        $rooms[] = $this->createRoom('202', $buildingRef);
+        $rooms[] = $this->createRoom('203', $buildingRef);
+        $rooms[] = $this->createRoom('204', $buildingRef);
+        $rooms[] = $this->createRoom('205', $buildingRef);
+        $rooms[] = $this->createRoom('301', $buildingRef);
+        $rooms[] = $this->createRoom('302', $buildingRef);
+        foreach ($rooms as $room) {
+            $manager->persist($room);
+        }
     }
 
     public function getDependencies()
